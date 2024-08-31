068b8d1b17f0cc12d779bfe04b39fe34f1c12ed9
Rocktim Saikia <rocktimthedev@gmail.com>
2024-09-01 03:51:36 +0530

    Update argparse descriptions and arguments

--- github_dl/console.py
+++ github_dl/console.py
@@ -7,14 +7,27 @@ from github_dl.source import main
 def cli():
     parser = argparse.ArgumentParser(
         prog="github-dl",
-        description="Download folders and files from Github",
+        description="Download folders and files from Github.",
         epilog="Thanks for using github-dl!",
     )
 
-    parser.add_argument("github_path", help="Github directory full URL path")
-    parser.add_argument("--output", help="Target directory to download to", default="")
     parser.add_argument(
-        "-v", "--version", action="version", version=f"%(prog)s {__version__}"
+        "github_path",
+        help="Github directory full URL path",
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="Destination directory to download to",
+        default="",
+        metavar="",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"%(prog)s {__version__}",
     )
 
     args = parser.parse_args()
